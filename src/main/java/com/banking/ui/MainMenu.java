e6b6cb931b1ae1cef3fc1f32e7348063b8563c72
HadiAlmubarak <hadi.almubarak@gmail.com>
2025-04-29 11:25:52 +0300

    Add admin, manager, and user menu functionalities with input handling

--- src/main/java/com/banking/ui/MainMenu.java
+++ src/main/java/com/banking/ui/MainMenu.java
@@ -1,5 +1,40 @@
 package com.banking.ui;
 
+import java.util.Scanner;
+
 public class MainMenu {
-    
+    public void handleMainMenu(){
+        // Main menu for the System
+        // It will display options to the user and handle their input
+        char input;
+        do {
+            displayMenu();
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine().charAt(0);
+            scanner.close();
+
+            switch (input) {
+                case '1':
+                    // Call method to create user
+                    break;
+                case '2':
+                    // Call method to login
+                    break;
+                case '3':
+                    System.out.println("Exiting the system. Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (input != '3');
+    }
+
+    public void displayMenu() {
+        System.out.println("Welcome to the Banking System");
+        System.out.println("1. Create User");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
+        System.out.print("Please select an option: ");
+    }
 }
--- src/main/java/com/banking/ui/UserMenu.java
+++ src/main/java/com/banking/ui/UserMenu.java
@@ -1,5 +1,49 @@
 package com.banking.ui;
 
+import java.util.Scanner;
+
 public class UserMenu {
-    
+    public void displayUserMenu() {
+        System.out.println("Welcome to the User Menu");
+        System.out.println("1. View Account Details");
+        System.out.println("2. Reset Password");
+        System.out.println("3. Deposit Money");
+        System.out.println("4. Withdraw Money");
+        System.out.println("5. Transfer Money");
+        System.out.println("6. Logout");
+        System.out.print("Please select an option: ");
+    }
+
+    public void handleUserMenu() {
+        char input;
+        // This method will handle user input and call the appropriate methods
+        do{
+            displayUserMenu();
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine().charAt(0);
+            scanner.close();
+            switch (input) {
+                case '1':
+                    // Call method to view account details
+                    break;
+                case '2':
+                    // Call method to deposit money
+                    break;
+                case '3':
+                    // Call method to withdraw money
+                    break;
+                case '4':
+                    // Call method to transfer money
+                    break;
+                case '5':
+                    // Call method to reset password
+                    break;
+                case '6':
+                    System.out.println("Logging out...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }while(input != '6');
+    }
 }
